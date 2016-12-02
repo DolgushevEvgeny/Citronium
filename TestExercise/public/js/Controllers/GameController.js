c573195cf7f6dc5a58fda210bbf565345b81c43b
Dolgushev Evgeny <baraxlush1995@gmail.com>
2016-12-02 13:51:20 +0300

    Доработки
    Добавлена развертка игрового поля на клиенте
    Добавлен метод для выполнения хода
    Добавлен метод для определения возможности сделать ход

--- TestExercise/public/js/Controllers/GameController.js
+++ TestExercise/public/js/Controllers/GameController.js
@@ -4,7 +4,8 @@ app.controller('GameController', ['$scope', 'ApiService', 'dataService', functio
         playerCode,
         canPlay,
         intervalHasPlayerJoin,
-        intervalCanIPlay;
+        intervalCanIPlay,
+        gameField;
 
     hasPlayerJoin = function() {
         var requestData = {};
@@ -22,58 +23,93 @@ app.controller('GameController', ['$scope', 'ApiService', 'dataService', functio
                     console.log(response.message);
                 }
             }
-
-            console.log(response.message);
         });
     };
 
-    makeMove = function() {
-        var requestData = {};
-        requestData.game_token = gameToken;
-        requestData.row = 1;
-        requestData.col = 2;
+    makeMove = function(row, column) {
+        //console.log("row: " + row);
+        //console.log("column: " + column);
 
-        ApiService.makeMove(accessToken).create(requestData).$promise.then(function(response) {
-            if (response.code == 8 && response.status != "error") {
+        if (canPlay) {
+            var requestData = {};
+            requestData.game_token = gameToken;
+            requestData.row = row;
+            requestData.col = column;
+
+            ApiService.makeMove(accessToken).create(requestData).$promise.then(function(response) {
+                if (response.code == 8 && response.status != "error") {
+                    canPlay = false;
+                    console.log(response.message);
+                    gameField = response.game_field;
+                    deployGameField();
+                    //todo спамить сервер пока игроку не разрешат сходить
+                    intervalCanIPlay = setInterval("canIPlay()", 4000);
+                } else if (response.code == 9 && response.status != "error") {
+                    canPlay = true;
+                    console.log(response.message);
+                } else {
+
+                }
+            });
+        }
+    };
+
+    canIPlay = function() {
+        var requestData = {};
+        console.log("gameToken " + gameToken);
+        requestData.gameToken = gameToken;
+
+        ApiService.canIPlay(accessToken).create(requestData).$promise.then(function(response) {
+            if (response.code == 4 && response.status != "error") {
+                canPlay = true;
+                console.log(response.message);
+                gameField = response.game_field;
+                deployGameField();
+                //todo перестать спамить сервер пока игроку не разрешат сходить
+                clearInterval(intervalCanIPlay);
+            } else if (response.code == 5 && response.status != "error") {
                 canPlay = false;
                 console.log(response.message);
                 //todo спамить сервер пока игроку не разрешат сходить
-                //intervalCanIPlay = setInterval("canIPlay()", 4000);
-            } else if (response.code == 9 && response.status != "error") {
-                canPlay = true;
-                console.log(response.message);
             } else {
 
             }
         });
+
     };
 
+    deployGameField = function() {
+        var gameTable = angular.element(document.querySelector(".table"));
+        for (var i = 0; i < gameField.length; ++i) {
+            var row = angular.element('<div></div>');
+            for (var j = 0; j < gameField[i].length; ++j) {
+                console.log("i: "+ i + " j: " + j);
+                var cell = angular.element('<div class="cell" onclick="makeMove(' + i + ',' + j + ')"></div>');
+                if (gameField[i][j] == 1) {
+                    cell.addClass('player');
+                } else if (gameField[i][j] == 2) {
+                    cell.addClass('ai');
+                }
+
+                row.append(cell);
+            }
+
+            gameTable.append(row);
+        }
+    };
 
     accessToken = dataService.getAccessToken();
     gameToken = dataService.getGameToken();
     playerCode = dataService.getPlayerCode();
+    gameField = dataService.getGameField();
 
     if (playerCode == 1) {
         var el = angular.element(document.querySelector("#gameToken"));
         el.text(gameToken);
+        deployGameField();
         intervalHasPlayerJoin = setInterval("hasPlayerJoin()", 4000);
     } else {
-
+        deployGameField();
+        intervalCanIPlay = setInterval("canIPlay()", 4000);
     }
-
-
-    //hasPlayerJoin();
-    //makeMove();
-
-
-    //insertElement = function() {
-    //    var newDiv = angular.element('<div id="inserted" onclick="alertMessage()">Жми сюда</div>');
-    //    var div = angular.element(document.querySelector("#insert"));
-    //    div.append(newDiv);
-    //};
-    //
-    //alertMessage = function() {
-    //    console.log("CRAP");
-    //};
-    //insertElement();
 }]);
